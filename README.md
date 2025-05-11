14900880b4e710a0dc5dd6c0f4deea818bf61f21
Pierogov <73725581+Pierogov@users.noreply.github.com>
2025-05-11 11:55:25 +0200

    Init repository
    new file:   README.md
    new file:   lcd_api.py
    new file:   main.py
    new file:   pico_i2c_lcd.py

---
+++ README.md
@@ -0,0 +1,3 @@
+# jumping-pico
+
+LCD API made with: https://github.com/T-622/RPI-PICO-I2C-LCD
---
+++ main.py
@@ -0,0 +1,31 @@
+from machine import I2C, Pin
+from time import sleep
+from pico_i2c_lcd import I2cLcd
+
+#Adress Pinout according to GPIO notation
+SDA_PIN = 0
+SCL_PIN = 1
+SWITCH_PIN = 28
+
+#init lcd
+i2c = I2C(0, sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), freq=400000)
+i2c_adress = i2c.scan()[0]
+lcd=I2cLcd(i2c, i2c_adress, 2, 16)
+
+#init button
+button = Pin(28, Pin.IN, Pin.PULL_DOWN)
+
+#prepare lcd for work
+lcd.clear()
+
+
+lcd.putstr("Przed")
+while True:
+    if(button.value()==1):
+        lcd.clear()
+        lcd.putstr("Po")
+        sleep(1)
+        lcd.clear()
+        lcd.putstr("Przed")
+
+
